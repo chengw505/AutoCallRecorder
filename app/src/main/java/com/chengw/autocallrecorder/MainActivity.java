1658c17e75978d8a21bef2d41835fe41cde72214
Cheng <chengw505@gmail.com>
2015-03-03 12:19:34 -0700

    1, bug fix 2, update icon

--- app/src/main/java/com/chengw/autocallrecorder/MainActivity.java
+++ app/src/main/java/com/chengw/autocallrecorder/MainActivity.java
@@ -1,9 +1,10 @@
 package com.chengw.autocallrecorder;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
@@ -11,26 +12,46 @@ import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
 public class MainActivity extends SlidingFragmentActivity {
 
+    private LeftSlidingMenuFragment mSlidingMenuFrag;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // initialize sliding menu
-        setBehindContentView(R.layout.menu_frame);
-        getSupportFragmentManager().beginTransaction().
-                replace(R.id.menu_frame, new LeftSlidingMenuFragment()).commit();
+        initSlidingMenu(savedInstanceState);
 
-        SlidingMenu menu = new SlidingMenu(this);
-        menu.setMode(SlidingMenu.LEFT);
-        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-        menu.setShadowWidthRes(R.dimen.shadow_width);
-        menu.setShadowDrawable(R.drawable.shadow);
-        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-        menu.setFadeDegree(0.35f);
-        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        // set flipper listener
+        ImageButton flipper = (ImageButton) findViewById(R.id.main_flipper);
+        flipper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggle();
+            }
+        });
     }
 
+    private void initSlidingMenu(Bundle savedInstanceState) {
+        // initialize sliding sm
+        setBehindContentView(R.layout.menu_frame);
+        if(null == savedInstanceState) {
+            android.support.v4.app.FragmentTransaction t = this.getSupportFragmentManager().beginTransaction();
+            mSlidingMenuFrag = new LeftSlidingMenuFragment();
+            t.replace(R.id.menu_frame, mSlidingMenuFrag);
+            t.commit();
+        } else {
+            mSlidingMenuFrag = (LeftSlidingMenuFragment) this.getSupportFragmentManager().findFragmentById(R.id.menu_frame);
+        }
+
+        SlidingMenu sm = getSlidingMenu();
+        sm.setShadowDrawable(R.drawable.shadow);
+        sm.setMode(SlidingMenu.LEFT);
+        sm.setFadeDegree(0.35f);
+        sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+        sm.setShadowWidthRes(R.dimen.shadow_width);
+        sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,10 +62,12 @@ public class MainActivity extends SlidingFragmentActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        if(id == R.id.main_flipper) {
+            toggle();
+            return true;
+        }
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
